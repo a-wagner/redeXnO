3e5d29d49ff454f9c9809141d4ca732c5b81b10d
Andrew Wagner <ahwagner@outlook.com>
2018-01-02 16:39:06 -0500

    add flat-count function

--- tic-tac-toe.rkt
+++ tic-tac-toe.rkt
@@ -17,3 +17,26 @@
   ([_ _ _]
    [_ _ _]
    [_ _ _]))
+
+; flat-count consumes an item and some variable number of expressions
+; and produces a number equal to the count of item in those expressions.
+; The expressions are flattened, so they may be individual items,
+; lists, lists of lists, etc.
+(define (flat-count item . exprs)
+  (count (curry equal? item) (flatten (append exprs))))
+
+(module+ test
+  (test-equal (flat-count "absolutely nuffin")
+              0)
+  (test-equal (flat-count 0 '())
+              0)
+  (test-equal (flat-count 0 '(1))
+              0)
+  (test-equal (flat-count 0 0 0)
+              2)
+  (test-equal (flat-count 0 '() '(0 1 0) '('(1 1) '(0 1 '(0 1 0))) 0)
+              6)
+  (test-equal (flat-count (term X) (term initial-board))
+              0)
+  (test-equal (flat-count (term X) (term ([X X X] [O O O] [X _ X _] [O O O O X])))
+              6))
